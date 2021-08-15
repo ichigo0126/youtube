37bd5b7a9127fe47c38f394e9d75a4d20f3b144c
酒巻昇馬 <ichigo@sakamakinoborubanoMacBook-Air.local>
2021-08-16 04:17:50 +0900

    youtube-react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import reportWebVitals from './serviceWorker';
+import { StoreProvider } from './store/index';
+import 'normalize.css'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
